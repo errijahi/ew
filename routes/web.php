18be9011a91ceb7e9082bbeecd8c728683d234da
errijahi <76177230+errijahi@users.noreply.github.com>
2024-07-26 11:23:29 +0200

    chore: remove unused code chore: remove unused code chore: removed unused code

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,4 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
-require __DIR__.'/socialstream.php';
+]);
