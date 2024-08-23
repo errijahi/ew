eb9edd90a968f77451e81f4bed54d4ce1b25ca81
errijahi <76177230+errijahi@users.noreply.github.com>
2024-08-23 10:26:01 +0200

    feat: added more dynamic dates for year and month filters in budget table

--- app/Filament/Resources/BudgetResource.php
+++ app/Filament/Resources/BudgetResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\BudgetResource\Pages;
 use App\Models\Category;
+use Exception;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
@@ -22,6 +23,9 @@ class BudgetResource extends Resource
 
     protected static ?string $label = 'Budget';
 
+    /**
+     * @throws Exception
+     */
     public static function table(Table $table): Table
     {
         return $table
@@ -41,12 +45,14 @@ class BudgetResource extends Resource
             ->filters([
                 SelectFilter::make('year')
                     ->label('Year')
-                    ->options([
-                        '2023' => '2023',
-                        '2024' => '2024',
-                        '2025' => '2025',
-                    ])
-                    ->default('2024')
+                    ->options(function () {
+                        $currentYear = date('Y');
+                        $startYear = '2000';
+                        $years = range($startYear, $currentYear + '10');
+
+                        return array_combine($years, $years);
+                    })
+                    ->default(date('Y'))
                     ->selectablePlaceholder(false)
                     ->indicateUsing(function (): ?string {
                         return null;
@@ -56,24 +62,27 @@ class BudgetResource extends Resource
                     }),
                 SelectFilter::make('month')
                     ->label('Month')
-                    ->options([
-                        '01' => 'January',
-                        '02' => 'February',
-                        '03' => 'March',
-                        '04' => 'April',
-                        '05' => 'May',
-                        '06' => 'June',
-                        '07' => 'July',
-                        '08' => 'August',
-                        '09' => 'September',
-                        '10' => 'October',
-                        '11' => 'November',
-                        '12' => 'December',
-                    ])
-                    ->default('01')  // Set the default month (e.g., January)
+                    ->options(function () {
+                        // Define all months
+                        return [
+                            '01' => 'January',
+                            '02' => 'February',
+                            '03' => 'March',
+                            '04' => 'April',
+                            '05' => 'May',
+                            '06' => 'June',
+                            '07' => 'July',
+                            '08' => 'August',
+                            '09' => 'September',
+                            '10' => 'October',
+                            '11' => 'November',
+                            '12' => 'December',
+                        ];
+                    })
+                    ->default(date('m'))
                     ->selectablePlaceholder(false)
                     ->indicateUsing(function (): ?string {
-                        return null;  // Hide the filter indicator
+                        return null;
                     })
                     ->query(function (Builder $query): Builder {
                         return $query;
