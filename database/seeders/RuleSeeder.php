8ac55f7f91692e4446c3041fae7ce0131a2feb25
errijahi <76177230+errijahi@users.noreply.github.com>
2024-08-30 12:09:56 +0200

    feat: added ruleSeeder

---
+++ database/seeders/RuleSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\IfAction;
+use App\Models\Rule;
+use App\Models\ThenAction;
+use Illuminate\Database\Seeder;
+
+class RuleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Rule::create([
+            'team_id' => 1,
+            'priority' => 10,
+            'stop_processing_other_rules' => false,
+            'delete_this_rule_after_use' => false,
+            'rule_on_transaction_update' => true,
+        ]);
+
+        IfAction::create([
+            'rule_id' => 1,
+            'category_id' => 1,
+        ]);
+
+        ThenAction::create([
+            'rule_id' => 1,
+            'tag_id' => 1,
+        ]);
+
+        //        Rule::factory()->count(20)->create();
+    }
+}
