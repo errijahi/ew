9a780681020b1fa6635a863c39127632c41999c9
errijahi <76177230+errijahi@users.noreply.github.com>
2025-01-28 06:39:12 +0100

    fix: login and register redirect in web

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', static function () {
     return redirect()->route('filament.admin.auth.login');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-]);
+Route::get('/login', static function () {
+    return redirect()->route('filament.admin.auth.login');
+});
+
+Route::get('/register', static function () {
+    return redirect()->route('filament.admin.auth.login');
+});
